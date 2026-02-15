4c9bab9dc941829a6a60d4774c7e3a3746def33e
yutaehun <aboutyu@gmail.com>
2026-02-15 23:47:18 +0900

    admin 메뉴 navigator 상태 완료

--- admin/src/helpers/intercepts/template-engine-variables.intercept.ts
+++ admin/src/helpers/intercepts/template-engine-variables.intercept.ts
@@ -19,33 +19,109 @@ export class TemplateVarsInterceptor implements NestInterceptor {
 
   // 메뉴 별 네비게이션 타이틀 설정
   private readonly naviagator = {
-    '/dashboard': { top_menu: '', sub_menu: '대시보드' },
-    '/users': { top_menu: '회원관리', sub_menu: '목록' },
-    '/users/detail': { top_menu: '회원관리', sub_menu: '상세' },
-    '/users/admin-users': { top_menu: '시스템관리', sub_menu: '어드민계정' },
+    '/dashboard': {
+      top_menu: '',
+      sub_menu: '대시보드',
+      menuState: 'dashboard',
+    },
+    '/users': {
+      top_menu: '회원관리',
+      sub_menu: '목록',
+      menuState: 'users',
+      subState: '',
+    },
+    '/users/detail': {
+      top_menu: '회원관리',
+      sub_menu: '상세',
+      menuState: 'users',
+      subState: '',
+    },
+    '/users/admin-users': {
+      top_menu: '시스템관리',
+      sub_menu: '어드민계정',
+      menuState: 'system',
+      subState: 'admin-users',
+    },
     '/users/admin-users/detail': {
       top_menu: '시스템관리',
       sub_menu: '어드민계정 상세',
+      menuState: 'system',
+      subState: 'admin-users',
     },
 
-    '/display/banner': { top_menu: '전시관리', sub_menu: '배너 등록' },
-    '/display/update-app': { top_menu: '전시관리', sub_menu: '앱버전관리' },
+    '/display/banner': {
+      top_menu: '전시관리',
+      sub_menu: '배너 등록',
+      menuState: 'display',
+      subState: 'banner',
+    },
+    '/display/update-app': {
+      top_menu: '전시관리',
+      sub_menu: '앱버전관리',
+      menuState: 'display',
+      subState: 'update-app',
+    },
+    '/cs/notice': {
+      top_menu: '고객센터',
+      sub_menu: '공지사항',
+      menuState: 'cs',
+      subState: 'notice',
+    },
+    '/cs/notice/detail': {
+      top_menu: '고객센터',
+      sub_menu: '공지사항 상세',
+      menuState: 'cs',
+      subState: 'notice',
+    },
+    '/cs/qna': {
+      top_menu: '고객센터',
+      sub_menu: '1:1문의',
+      menuState: 'cs',
+      subState: 'qna',
+    },
+    '/cs/faq/detail': {
+      top_menu: '고객센터',
+      sub_menu: 'FAQ 상세',
+      menuState: 'cs',
+      subState: 'faq',
+    },
+    '/cs/faq': {
+      top_menu: '고객센터',
+      sub_menu: 'FAQ',
+      menuState: 'cs',
+      subState: 'faq',
+    },
 
-    '/cs/notice': { top_menu: '고객센터', sub_menu: '공지사항' },
-    '/cs/notice/detail': { top_menu: '고객센터', sub_menu: '공지사항 상세' },
-    '/cs/qna': { top_menu: '고객센터', sub_menu: '1:1문의' },
-    '/cs/faq/detail': { top_menu: '고객센터', sub_menu: 'FAQ 상세' },
-    '/cs/faq': { top_menu: '고객센터', sub_menu: 'FAQ' },
-
-    '/system/adminuser': { top_menu: '시스템관리', sub_menu: '어드민계정' },
-    '/system/eula': { top_menu: '시스템관리', sub_menu: '약관등록' },
+    '/system/eula': {
+      top_menu: '시스템관리',
+      sub_menu: '약관등록',
+      menuState: 'system',
+      subState: 'eula',
+    },
     '/system/eula/detail': {
       top_menu: '시스템관리',
       sub_menu: '약관등록 상세',
+      menuState: 'system',
+      subState: 'eula',
     },
-    '/system/serviceInfo': { top_menu: '시스템관리', sub_menu: '서비스 정보' },
-    '/system/rawdata': { top_menu: '시스템관리', sub_menu: '기초데이터' },
-    '/system/devices': { top_menu: '시스템관리', sub_menu: '등록디바이스' },
+    '/system/serviceInfo': {
+      top_menu: '시스템관리',
+      sub_menu: '서비스 정보',
+      menuState: 'system',
+      subState: 'serviceInfo',
+    },
+    '/system/rawdata': {
+      top_menu: '시스템관리',
+      sub_menu: '기초데이터',
+      menuState: 'system',
+      subState: 'rawdata',
+    },
+    '/system/devices': {
+      top_menu: '시스템관리',
+      sub_menu: '등록디바이스',
+      menuState: 'system',
+      subState: 'devices',
+    },
   };
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
@@ -66,7 +142,11 @@ export class TemplateVarsInterceptor implements NestInterceptor {
             adminCreateTime: session.adminCreateTime || '-',
           },
           service,
-          navigator: this.naviagator[urlPath] || { top_menu: '', sub_menu: '' },
+          navigator: this.naviagator[urlPath] || {
+            top_menu: '',
+            sub_menu: '',
+            menuState: '',
+          },
         };
 
         return next.handle().pipe(
