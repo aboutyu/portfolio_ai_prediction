3e25ab5fe27083946f96426bcaa9f5846c45972c
yutaehun <aboutyu@gmail.com>
2026-03-05 00:38:56 +0900

    dockerfile 수정

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,22 +1,20 @@
-import { Get, Injectable, Query } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import * as path from 'path';
 
 @Injectable()
 export class AppService {
-    
-
   getHello(lang?: string): string {
-      const getFilePath = () => {
-          if (lang === 'en') {
-              return path.join(__dirname, '/resources/resume/en.html');            
-          } else if (lang === 'ko') {
-              return path.join(__dirname, '/resources/resume/ko.html');
-          } else {
-              return path.join(__dirname, '/resources/hello.html');
-          }
+    const getFilePath = () => {
+      if (lang === 'en') {
+        return path.join(__dirname, '/resources/resume/en.html');
+      } else if (lang === 'ko') {
+        return path.join(__dirname, '/resources/resume/ko.html');
+      } else {
+        return path.join(__dirname, '/resources/hello.html');
       }
-      const htmlContent = fs.readFileSync(getFilePath(), 'utf8');
-      return htmlContent;
+    };
+    const htmlContent = fs.readFileSync(getFilePath(), 'utf8');
+    return htmlContent;
   }
 }
